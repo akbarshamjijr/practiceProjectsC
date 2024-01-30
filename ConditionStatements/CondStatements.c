f323e3648076db44c324c795a2d2cb9359ad910a
akbarshamjijr <akbar_shamji@brown.edu>
2024-01-30 08:20:56 -0500

    Condition statements

---
+++ ConditionStatements/CondStatements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+	int x = 42;
+
+	if(x >= 40){
+
+		printf("The variable is in range");
+	}
+	else if(x == 0){
+		printf("x is 0...");
+	}
+	else{
+		printf("The varaible is not in range");
+	}
+
+
+	return 0;
+}
