1932f7f0f73d70a0883ef99fe0e868c01944bb27
akbarshamjijr <akbar_shamji@brown.edu>
2024-02-24 14:51:39 -0500

    signedness

---
+++ signedness/signedness.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+	//unsigned means you can only represent positive numbers.
+	//the sign bit, the 16th bit will display signedness
+	//with signed integers, we can only represent 2^15 - 1
+
+	
+
+
+
+
+
+	return 0;
+
+
+}
